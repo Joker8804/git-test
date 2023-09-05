79e31705e1d274e368b0efbc81b3e83e94c35416
lawxu <269787143@qq.com>
2023-09-05 11:41:55 +0800

    第一次commit

---
+++ src/main/java/com/lawxu/GitTest.java
@@ -0,0 +1,13 @@
+package com.lawxu;
+
+/**
+ * @program: git-test
+ * @description:
+ * @author: LawXu
+ * @create: 2023-09-05 11:39
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
